109b671a7ab38b71438e64dde3f7742998d3cf7c
r2soft <paulhana6006@gmail.com>
2023-09-12 14:34:12 +0900

    security 테스트코드 추가

---
+++ src/test/java/me/kktrkkt/springsecurityexample/simple/SimpleControllerTest.java
@@ -0,0 +1,72 @@
+package me.kktrkkt.springsecurityexample.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SimpleControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithAnonymousUser
+    void main_anonymous() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @WithUser
+    void main_user() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("user")))
+                .andDo(print());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void dashboard_anonymous() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/dashboard"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"))
+                .andDo(print());
+    }
+
+    @Test
+    @WithUser
+    void dashboard_user() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/dashboard"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @WithAdmin
+    void admin_admin() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/admin"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @WithUser
+    void admin_user() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/admin"))
+                .andExpect(status().isForbidden())
+                .andDo(print());
+    }
+
+}
