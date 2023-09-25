de74de45ba6da0c49531f556012d1ec445c876ce
r2soft <paulhana6006@gmail.com>
2023-09-25 17:50:26 +0900

    security async 예제 추가

---
+++ src/main/java/me/kktrkkt/springsecurityexample/async/AsyncController.java
@@ -0,0 +1,31 @@
+package me.kktrkkt.springsecurityexample.async;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.Callable;
+
+@Controller
+@RequestMapping("/async")
+public class AsyncController {
+
+    @GetMapping
+    @ResponseBody
+    public Callable<String> getAsync() {
+        log("getAsync");
+        return () -> {
+            log("Callable");
+            return "hello world";
+        };
+    }
+
+    private void log(String name) {
+        System.out.println(name);
+        System.out.println("Thread: " + Thread.currentThread());
+        System.out.println("Principal: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    }
+
+}
