a978def3dbae157a0676188cf2a98d6595c4efb3
r2soft <paulhana6006@gmail.com>
2023-09-26 11:12:07 +0900

    CsrfFilter

--- src/main/java/me/kktrkkt/springsecurityexample/security_filter/package-info.java
+++ src/main/java/me/kktrkkt/springsecurityexample/security_filter/package-info.java
@@ -10,5 +10,6 @@ HeaderWriterFilter: 시큐리티 관련 헤더를 추가해준다. 마임 타입
     - 캐시 히스토리 취약점 방어: [Cache-Control: no-cache, no-store, max-age=0, must-revalidate], [Expires: 0], [Pragma: no-cache]
     - HTTPS로만 소통하도록 강제: Strict-Transport-Security: max-age=31536000; includeSubDomains
     - clickjacking 방어: X-XSS-Protection: 1; mode=block
+CsrfFilter: CSRF(사이트간 요청 위조) 공격을 방지한다. csrf 토큰을 통해 방지한다, http().csrf().disable()로 비활성화 가능
 
  */
