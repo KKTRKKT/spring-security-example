ba5118575910a79ca5498da6e5955aed8664d1eb
r2soft <paulhana6006@gmail.com>
2023-09-26 10:27:14 +0900

    WebAsyncManagerIntegrationFilter

--- src/main/java/me/kktrkkt/springsecurityexample/security_filter/package-info.java
+++ src/main/java/me/kktrkkt/springsecurityexample/security_filter/package-info.java
@@ -2,3 +2,4 @@ package me.kktrkkt.springsecurityexample.security_filter;
 
 /*
 DisableEncodeUrlFilter: 세션 ID가 URL에 노출되는것을 방지한다.
+WebAsyncManagerIntegrationFilter: 핸들러에서 Callable을 사용할 시 Callable 내에서 SecurityContext를 사용할 수 있도록 설정해준다.
