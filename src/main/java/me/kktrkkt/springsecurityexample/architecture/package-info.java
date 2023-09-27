0d90fa6477c8239690ca7bd541e36d2a4526d52d
r2soft <paulhana6006@gmail.com>
2023-09-27 10:00:25 +0900

    UsernamePasswordAuthenticationFilter

--- src/main/java/me/kktrkkt/springsecurityexample/architecture/package-info.java
+++ src/main/java/me/kktrkkt/springsecurityexample/architecture/package-info.java
@@ -14,7 +14,7 @@ SecurityContextHolder: SecurityContext를 담고 있는 객체이며, 기본적�
 AuthenticationManager: 하나 이상의 AuthenticationProvider를 통해 Authentication이 유효한지 확인하는 인터페이스로 ProviderManager가 구현한다.
                        처리할 수 없으면 부모 ProviderManager를 호출해 위 과정을 반복한다
     - AuthenticationProvider: 인증할 수 있는 Authencation 확인 로직관 실제 인증이 처리되는 로직이 담겨있다.
-                              인증시 UserDetailsService에서 UserDetails 정보를 가져와서 사용자가 입력한 정보(username, password)와 비교해 인증하게 된다.
+                              AuthenticationProvider 중 DaoAuthenticationProvider에 의해 UserDetailsService에서 UserDetails 정보를 가져와서 사용자가 입력한 정보(username, password)와 비교해 인증하게 된다.
                               인증이 유효하면 Autehncation의 Principal에 유저 정보를 담아서 반환하고 인증이 유효하지 않으면 비활성화 계정, 잘못된 비번, 잠긴 계정등의 예외를 던진다
 
 UsernamePasswordAuthenticationFilter: 아이디와 비밀번호로 로그인할 때 동작하는 인증 필터다.
