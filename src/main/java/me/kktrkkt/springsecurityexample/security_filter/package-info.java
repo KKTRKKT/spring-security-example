3c7ca5b9a9671f7b61b411a7d9eee922efc09bcb
r2soft <paulhana6006@gmail.com>
2023-09-27 10:07:03 +0900

    LogoutFilter

--- src/main/java/me/kktrkkt/springsecurityexample/security_filter/package-info.java
+++ src/main/java/me/kktrkkt/springsecurityexample/security_filter/package-info.java
@@ -11,6 +11,6 @@ HeaderWriterFilter: 시큐리티 관련 헤더를 추가해준다. 마임 타입
     - HTTPS로만 소통하도록 강제: Strict-Transport-Security: max-age=31536000; includeSubDomains
     - clickjacking 방어: X-XSS-Protection: 1; mode=block
 CsrfFilter: CSRF(사이트간 요청 위조) 공격을 방지한다. csrf 토큰을 통해 방지한다, http().csrf().disable()로 비활성화 가능
-LogoutFilter: 로그아웃을 담당하는 필터로 로그아웃 url, 로그아웃 처리, 로그아웃 성공 후 처리 및 url 설정 등을 담당한다
+LogoutFilter: 로그아웃을 담당하는 필터로 로그아웃 url, 로그아웃 처리, 로그아웃 성공 후 처리 및 url 설정 등을 담당한다. (http.logout()을 통해 설정)
 UsernamePasswordAuthenticationFilter: 아이디와 비밀번호로 로그인할 때 동작하는 인증 필터다. 자세한건 architecture.pakcage-info 참고
  */
